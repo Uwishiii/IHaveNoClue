da4a63e5835ce2a160cb0c7b2827ad006cad77f6
VikiST68 <152392508+VikiST68@users.noreply.github.com>
2024-10-09 00:48:54 +0300

    Puzzel fixed

---
+++ Assets/ToogleG.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.EditorTools;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class ToogleG : MonoBehaviour
+{
+
+public XRInteractableEvent XRGrabInteractable;
+
+void Update()
+{
+   if (Input.GetKey(KeyCode.G))
+   {
+       //XRGrabInteractable;
+   }
+}
+
+}
